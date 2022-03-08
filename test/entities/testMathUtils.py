de33fc6fd700fc695e163120ec1a768ec90c5df9
NathanDuPont <nathan.dupont01@gmail.com>
2022-03-07 19:58:35 -0600

    Adding logic to support testing through python unittest

---
+++ test/entities/testMathUtils.py
@@ -0,0 +1,13 @@
+import unittest
+# from entities.mathUtils import Pose2D, distance_between_points
+
+class TestMathUtils(unittest.TestCase):
+    def test_pose2d(self):
+        self.assertTrue(True)
+
+    def test_distance_between_points(self):
+        self.assertTrue(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
