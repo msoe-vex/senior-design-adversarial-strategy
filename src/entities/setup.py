24d05c98047d15d336ea0049ae3ecdac0e0d353a
ChipHennig <chip.hennig@gmail.com>
2022-04-01 22:11:32 -0500

    package setup

---
+++ src/entities/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='vexentities',
+    version='1.0',
+    packages=['.'],
+    url='https://github.com/msoe-vex/senior-design-adversarial-strategy',
+    license='',
+    author='NathanDuPont',
+    author_email='dupontn@msoe.edu',
+    description='VEX Tipping Point entity classes'
+)
